3f58f9dcfcc19bca7ac4cf02d165fd13a8e9ad46
ayzel <andrewzlee@gmail.com>
2024-02-18 22:31:15 -0500

    finish lc template

--- src/character_logic/light_cones/LCTemplate.ts
+++ src/character_logic/light_cones/LCTemplate.ts
@@ -1,6 +1,11 @@
 import type { ILCStatPage } from "../stat_logic";
+import lcJSON from '../data/hsr_lc_stats.json';
 
 class LCTemplate implements ILCStatPage {
+  public baseATK: number = -1;
+  public baseDEF: number = -1;
+  public baseHP: number = -1;
+  
   constructor (
     // implementing interface
     public lcLevel: number,
@@ -10,7 +15,16 @@ class LCTemplate implements ILCStatPage {
     public lcName: string,
   ) {
     // get the base stats for LC
-    
+    const lcStats = lcJSON.find(json => json['Name'] === this.lcName);
+    const levelKey = this.lcLevel.toString();
+    if (lcStats) {
+      const atkObject = lcStats.Stats.ATK as { [key: string] : number };
+      const defObject = lcStats.Stats.DEF as { [key: string] : number };
+      const hpObject = lcStats.Stats.HP as { [key: string] : number };
+      this.baseATK = atkObject[levelKey];
+      this.baseDEF = defObject[levelKey];
+      this.baseHP = hpObject[levelKey];
+    }
   }
 }
 
