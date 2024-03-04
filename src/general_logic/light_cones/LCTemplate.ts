3c09e2a7a073497793c91a3ccc721e8dd947ac08
ayzel <andrewzlee@gmail.com>
2024-03-04 10:28:25 -0500

    update LC template
    needed to update the way I was reading LC data from the json since the new scraper structures the json differently

--- src/general_logic/light_cones/LCTemplate.ts
+++ src/general_logic/light_cones/LCTemplate.ts
@@ -2,6 +2,22 @@ import type { ILCStatPage } from "../stat_logic";
 import lcJSON from '../../data/hsr_lc_stats.json';
 import { Path } from "../enums";
 
+interface IndividualLCStats {
+  Name: string;
+  ImageLink: string;
+  BaseStats: {
+    [key: string]: {
+      ATK: number;
+      DEF: number;
+      HP: number;
+    }
+  }
+}
+
+interface LCJSON {
+  [key: string]: IndividualLCStats;
+}
+
 export default class LCTemplate implements ILCStatPage {
   public baseATK: number = -1;
   public baseDEF: number = -1;
@@ -14,19 +30,19 @@ export default class LCTemplate implements ILCStatPage {
     public superimpositionLevel: number,
 
     // LC unique data
-    public lcName: string,
+    public lcID: string,
     public lcPath: Path,
   ) {
-    // get the base stats for LC
-    const lcStats = lcJSON.find(json => json['Name'] === this.lcName);
+    // read json
+    const typedLCJSON = lcJSON as LCJSON;
+
+    // get base stats
+    const individualLCStats: IndividualLCStats = typedLCJSON[this.lcID]
     const levelKey = this.lcLevel.toString();
-    if (lcStats) {
-      const atkObject = lcStats.Stats.ATK as { [key: string] : number };
-      const defObject = lcStats.Stats.DEF as { [key: string] : number };
-      const hpObject = lcStats.Stats.HP as { [key: string] : number };
-      this.baseATK = atkObject[levelKey];
-      this.baseDEF = defObject[levelKey];
-      this.baseHP = hpObject[levelKey];
+    if (individualLCStats) {
+      this.baseATK = individualLCStats.BaseStats[levelKey]['ATK'];
+      this.baseDEF = individualLCStats.BaseStats[levelKey]['DEF'];
+      this.baseHP = individualLCStats.BaseStats[levelKey]['HP'];
     }
   }
 }
