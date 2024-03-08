17cceb5837ee8863c69ab91dba4086ba1a989d37
ayzel <andrewzlee@gmail.com>
2024-03-08 09:42:39 -0500

    add some more defaults

--- src/general_logic/defaults/DefaultRelicSet.ts
+++ src/general_logic/defaults/DefaultRelicSet.ts
@@ -8,7 +8,34 @@ const defaultRelicSet = { // key = char ID, value = light cone ID
       'feetStat': 'speed',
       'orbStat': 'lightningDamage',
       'ropeStat': 'attackPercent',
-    }, // Acheron and 4pc Pioneer
+    }, // Acheron and 4pc Pioneer with 2pc Izumo Gensei
+    '1307': {
+      'twoPieceID': '116',
+      'fourPieceID': '116',
+      'planarID': '311',
+      'bodyStat': 'effectHitRate',
+      'feetStat': 'speed',
+      'orbStat': 'windDamage',
+      'ropeStat': 'attackPercent',
+    }, // Black Swan with 4pc Prisoner and 2pc Glamoth
+    '1306': {
+      'twoPieceID': '114',
+      'fourPieceID': '106',
+      'planarID': '310',
+      'bodyStat': 'critDamage',
+      'feetStat': 'speed',
+      'orbStat': 'hpPercent',
+      'ropeStat': 'energyRegen'
+    }, // Sparkle with 2pc Guard, 2pc Messenger, and 2pc Broken Keel
+    '1305': {
+      'twoPieceID': '117',
+      'fourPieceID': '117',
+      'planarID': '314',
+      'bodyStat': 'critDamage',
+      'feetStat': 'speed',
+      'orbStat': 'imaginaryDamage',
+      'ropeStat': 'attackPercent'
+    }
   }
   
   export default defaultRelicSet;
